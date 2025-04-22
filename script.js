d87fe5b0e75c05e93336042c8b49fd18056ad528
ColonelAtlas <tidiane.ndiouck19@gmail.com>
2025-04-22 07:27:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* function myFunction() {
+  alert("⛔ Site en construction ⛔");
+}
+
+window.onload = function () {
+  myFunction();
+}; */
+document.getElementById("voir1").onclick = function() {
+  window.location.href = "soins.html"; // Remplace par l'URL souhaitée
+};
